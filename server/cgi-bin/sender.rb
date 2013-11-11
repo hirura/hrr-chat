2bd55c45e417cde9130c3665a35a6ce95aeafddf
hirura <hirura@gmail.com>
2013-11-12 00:13:57 +0900

    fixed URIencoding

--- server/cgi-bin/sender.rb
+++ server/cgi-bin/sender.rb
@@ -1,19 +1,18 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
+require 'cgi'
 require 'socket'
 
 def sender name, line, host='localhost', port=12347
 	s = TCPSocket.new host, port
-	s.puts Marshal.dump( [name, line] )
+	s.puts Marshal.dump( [CGI.unescape( name ), CGI.unescape( line )] )
 	s.close
 end
 
 if __FILE__ == $0
-	require 'cgi'
-
 	print "Content-Type: text/html; charset=utf-8\n\n"
 
 	input = CGI.new
-	sender CGI.unescape( input["name"] ), CGI.unescape( input["line"] )
+	sender input["name"], input["line"]
 end
